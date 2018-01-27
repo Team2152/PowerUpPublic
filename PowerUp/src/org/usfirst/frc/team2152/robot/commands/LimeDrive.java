93080059e1f6d62af7f2c3dd3c7611de69a17eac
Kenneth <Kenneth@10.153.103.25>
2018-01-27 11:12:32 -0500

    Added LimeDrive.java

---
+++ PowerUp/src/org/usfirst/frc/team2152/robot/commands/LimeDrive.java
@@ -0,0 +1,56 @@
+package org.usfirst.frc.team2152.robot.commands;
+
+import org.usfirst.frc.team2152.robot.Robot;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * Takes input from the joystick and outputs it to the motors, PID turning and
+ * heading hold available.
+ */
+
+
+// Needs to tune for new robot/carpet
+public class LimeDrive extends Command {
+
+	public LimeDrive() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.driveTrainSubsystem);
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		double outputThrottle = 0;
+		double outputTurn = 0;
+		outputThrottle = Robot.m_oi.driverXbox.getRawAxis(1);
+		outputTurn = Robot.m_oi.driverXbox.getRawAxis(4);
+		if (Robot.m_oi.driverXbox.getRawButton(5) || (Math.abs(outputThrottle) <= 0.25)) {
+			Robot.driveTrainSubsystem.arcadeDrive(0.0, (outputTurn * 0.66));
+		} else {
+			Robot.driveTrainSubsystem.arcadeDrive((outputThrottle), -(outputThrottle * outputTurn));
+		}
+	}
+
+
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+
+		Robot.driveTrainSubsystem.tankDrive(0, 0);
+	}
+}
