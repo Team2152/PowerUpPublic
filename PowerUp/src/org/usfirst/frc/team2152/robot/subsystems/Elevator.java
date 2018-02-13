48407f61e6ef5d84505c04a5f728a4c3115c7fd1
Nathan Rose <mail.nathanrose@gmail.com>
2018-02-13 15:16:34 -0500

    basic code

---
+++ PowerUp/src/org/usfirst/frc/team2152/robot/subsystems/Elevator.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team2152.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Elevator extends Subsystem{
+
+	/*
+	 * you need to:
+	 * 		create motors
+	 * 		create any ports (i.e. limitSwitch = RobotMap.limitSwitch)
+	 * 		create a constructor that can create a Elevator object
+	 * 			-in this initialize the motors
+	 * 		create any methods you will need(i.e. move(double inches) )
+	 * 
+	 * 		***if you need help ask for help or look at previous code***
+	 */
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
