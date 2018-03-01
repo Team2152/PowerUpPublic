6c750154408c621533d3dc382fb4df4e4cb0ae56
pothocket <pothocket1@gmail.com>
2018-03-01 16:14:02 +0000

    Incorporate actual encoder values and UDP constants from RobotMap

--- PowerUp/src/org/usfirst/frc/team2152/robot/subsystems/EncoderSendSystem.java
+++ PowerUp/src/org/usfirst/frc/team2152/robot/subsystems/EncoderSendSystem.java
@@ -1,21 +1,27 @@
 package org.usfirst.frc.team2152.robot.subsystems;
 
+import org.usfirst.frc.team2152.robot.Robot;
+import org.usfirst.frc.team2152.robot.RobotMap;
 import org.usfirst.frc.team2152.robot.network.UDPClient;
 import org.usfirst.frc.team2152.robot.utilities.ByteUtil;
 import org.usfirst.frc.team2152.robot.utilities.Util;
 
+/**
+ * Continuously sends cumulative left and right encoder ticks to lidar odroid with timestamps
+ * @author David
+ */
 public class EncoderSendSystem extends Thread {
 
 	private boolean running = false; 
 	private int hertz;
 
 	private UDPClient out;
-	int port;
+	int remotePort;
 
-	public EncoderSendSystem (int port, int hertz) {
+	public EncoderSendSystem (int remotePort, int hertz) {
 		out = new UDPClient();
 		this.hertz = hertz;
-		this.port = port;
+		this.remotePort = remotePort;
 	}
 
 	public void run() {
@@ -27,16 +33,16 @@ public class EncoderSendSystem extends Thread {
 			if(delta < 1000 / hertz)
 				continue;
 
-			System.out.println("Sending encoder data");
-			System.out.println(System.nanoTime());
+			//System.out.println("Sending encoder data");
+			//System.out.println(System.nanoTime());
 
-			int encoderL = 47;
-			int encoderR = 123456;
+			int encoderL = Robot.driveTrainSubsystem.getEncoderRTicksCumulative();
+			int encoderR = Robot.driveTrainSubsystem.getEncoderLTicksCumulative();
 			
 			out.send( Util.concatBytes( ByteUtil.toByteArrayLE(encoderL)
 									  , ByteUtil.toByteArrayLE(encoderR)
 									  , ByteUtil.toByteArrayLE(System.nanoTime()))
-					, "10.21.52.12", port);
+					, RobotMap.LIDAR_ODROID_IP, remotePort);
 
 			delta = 0;
 			lastTime = System.currentTimeMillis();
